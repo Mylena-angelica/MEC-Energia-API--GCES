db8ffc8e331261fefe21d59bc3822332ab281e9f
Mylena Angélica <mylenangelica@gmail.com>
2024-08-29 09:49:43 -0300

    Add files via upload

---
+++ users/authentications.py
@@ -0,0 +1,293 @@
+
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes, permission_classes
+from drf_yasg.utils import swagger_auto_schema
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.tokens import default_token_generator
+
+from . import serializers
+from .requests_permissions import RequestsPermissions
+
+from utils.user.user_type_util import UserType
+from utils.endpoints_util import EndpointsUtils
+from utils.user.authentication import create_token_response, create_valid_token_response, generate_link_to_reset_password, generate_random_password
+from utils.email.send_email import send_email_first_access_password, send_email_reset_password, send_email_reset_password_by_admin
+
+from users.models import UserToken, CustomUser
+
+class Authentication(ObtainAuthToken):
+
+    @swagger_auto_schema(responses={200: serializers.AuthenticationTokenSerializerForDocs()})
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = self.serializer_class(data=request.data,
+                                            context={'request': request})
+
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data['user']
+
+            if not user.account_password_status in ['OK', 'user_reset']:
+                raise Exception('Usuário não pode fazer login no sistema')
+            
+            try:
+                existing_token = Token.objects.get(user=user)
+                existing_token.delete()
+            except Token.DoesNotExist:
+                pass
+
+            token = Token.objects.create(user=user)
+        except Exception as error:
+            return Response({'Authentication error': f'{str(error)}'}, status.HTTP_401_UNAUTHORIZED)
+
+        try:
+            UserType.is_valid_user_type(user.type)
+            response = Authentication._create_and_update_login_response(token.key, user.id, user.email, user.first_name, user.last_name, user.type)
+
+            return Response(response)
+        except Exception as error:
+            return Response({'Authentication error': f'{str(error)}'}, status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(query_serializer=serializers.AuthenticationGetTokenParamsSerializer,
+                        responses={200: serializers.AuthenticationGetTokenSerializerForDocs()})
+    def get(self, request, *args, **kwargs):
+        params_serializer = serializers.AuthenticationGetTokenParamsSerializer(data=request.data)
+        if not params_serializer.is_valid():
+            return Response(params_serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        try:
+            Token.objects.get(pk=request.data['token'])
+
+            is_valid_token = True
+        except ObjectDoesNotExist:
+            is_valid_token = False
+
+        response = create_valid_token_response(is_valid_token)
+
+        return Response(response)
+    
+    def _invalid_sessions_tokens(user):
+        sessions_tokens = Token.objects.filter(user=user)
+
+        if sessions_tokens:
+            sessions_tokens.delete()
+
+    def _create_and_update_login_response(token, user_id, user_email, user_first_name, user_last_name, user_type):
+        response = create_token_response(token, user_id, user_email, user_first_name, user_last_name, user_type)
+
+        if user_type in RequestsPermissions.university_user_permissions:
+            user = RequestsPermissions.get_university_user_object(user_id)
+            university_id = user.university.id
+
+            response = Authentication._update_university_user_response(response, university_id)
+
+        return response
+
+    def _update_super_user_response(response):
+        return response
+
+    def _update_university_user_response(response, university_id):
+        response = Authentication._insert_university_id_on_response(response, university_id)
+        
+        return response
+
+    def _insert_university_id_on_response(response, university_id):
+        response['user']['universityId'] = university_id
+
+        return response
+
+
+class Logout(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            token = Token.objects.get(user=request.user)
+            token.delete()
+
+            return Response({"detail": "Logout successful"}, status=status.HTTP_200_OK)
+        except Token.DoesNotExist:
+            return Response({"detail": "Token not found"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"detail": f"Failed to logout: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class Password():
+    def generate_password_token(user):
+        Password._invalid_all_generated_password_tokens(user)
+        
+        token = default_token_generator.make_token(user = user)
+        UserToken.objects.create(user = user, token = token)
+
+        return token
+
+    def generate_link_to_reset_password(user: str, token: str or None, password_status: str):
+        if not token:
+            raise Exception('Is necessary a password token')
+
+        Password._get_user_by_token(token)
+
+        return generate_link_to_reset_password(token, user.first_name, password_status)
+
+    def check_password_token_is_valid(user, token):
+        return default_token_generator.check_token(user, token)
+
+    def change_user_password(user_new_password, token):
+        try:
+            user = Password._get_user_by_token(token)
+
+            user.change_user_password_by_reset_password_token(user_new_password, token)
+            user.set_account_password_status_to_ok()
+
+            Password._invalid_all_generated_password_tokens(user)
+        except Exception as error:
+            raise Exception('Change user password: ' + str(error))
+    
+    def _get_user_by_token(token):
+        return UserToken.get_user_by_token(token)
+
+    def _invalid_all_generated_password_tokens(user):
+        UserToken.objects.filter(user=user).delete()
+    
+    def send_email_reset_password_by_admin(email):
+        try:
+            user = CustomUser.search_user_by_email(email = email)
+            
+            user.account_password_status = 'admin_reset'
+            user.save()
+
+            user.set_password(generate_random_password())
+
+            Authentication._invalid_sessions_tokens(user)
+
+            token = Password.generate_password_token(user)
+            link = Password.generate_link_to_reset_password(user, token, 'admin_reset')
+            
+            send_email_reset_password_by_admin(user.first_name, user.email, link)
+        except Exception as error:
+            raise Exception('Send email reset password by admin: ' + str(error))
+
+    def send_email_reset_password(email):
+        try:
+            user = CustomUser.search_user_by_email(email = email)
+
+            user.account_password_status = 'user_reset'
+            user.save()
+
+            token = Password.generate_password_token(user)
+            link = Password.generate_link_to_reset_password(user, token, 'user_reset')
+            
+            send_email_reset_password(user.first_name, user.email, link)
+        except CustomUser.DoesNotExist:
+            pass
+        except Exception as error:
+            raise Exception('Send email reset password: ' + str(error))
+
+    def send_email_first_access_password(user):
+        try:
+            user.account_password_status = 'first_access'
+            user.save()
+
+            Authentication._invalid_sessions_tokens(user)
+
+            token = Password.generate_password_token(user)
+            link = Password.generate_link_to_reset_password(user, token, 'first_access')
+            
+            send_email_first_access_password(user.first_name, user.university.acronym, user.email, link)
+        except Exception as error:
+            raise Exception('Send email first access password: ' + str(error))
+
+
+class ResetPasswordByAdmin(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        request_user = request.user
+
+        if request_user.type not in ['super_user', 'university_admin']:
+            raise Exception('Esse usuário não tem permissão para executar essa ação')
+
+        user_id = int(request.GET.get('user_id'))
+        user_for_reset = CustomUser.search_user_by_id(user_id)
+
+        if request_user.id == user_for_reset.id:
+            raise Exception('Utilize o esqueci minha senha')
+        
+        Password.send_email_reset_password_by_admin(user_for_reset.email)
+
+        return Response({"message": "Enviado"})
+
+
+code_password_token_ok = 1 # Usuário tem um Password Token válido
+code_password_token_expired = 2 # Email seja agendado com um novo Password Token
+@authentication_classes([])
+@permission_classes([])
+class ResetPassword(generics.GenericAPIView):
+    @swagger_auto_schema(query_serializer=serializers.ResetPasswordParamsSerializer,
+                         responses={200: serializers.ResetPasswordParamsForDocs})
+    def post(self, request, *args, **kwargs):
+        try:
+            request_user_email = request.GET.get('email')
+
+            Password.send_email_reset_password(request_user_email)
+
+            response = EndpointsUtils.create_message_endpoint_response(
+                        status = EndpointsUtils.status_success, 
+                        message = "O email foi enviado para o usuário com link de redefinição de senha")
+
+            return Response(response, status.HTTP_200_OK)
+        except Exception as error:
+            response = EndpointsUtils.create_message_endpoint_response(
+                        status = EndpointsUtils.status_error,
+                        message = str(error))
+
+            return Response(response, status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            request_token = request.GET.get('token')
+
+            user, code_token = UserToken.get_user_by_token_and_set_invalid_tried(request_token)
+            
+            response = {
+                "status": EndpointsUtils.status_success,
+                "code": code_token,
+                "message": "Token válido" if code_token == code_password_token_ok else "Novo email será enviado",
+                "email": user.email,
+            }
+                
+            return Response(response, status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": f"Error: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@authentication_classes([])
+@permission_classes([])
+class ConfirmResetPassword(generics.GenericAPIView):
+    @swagger_auto_schema(request_body=serializers.ConfirmPasswordBodySerializer,
+                         responses={200: serializers.ResetPasswordParamsForDocs})
+    def post(self, request, *args, **kwargs):
+        try:
+            request_user_new_password = request.data['user_new_password']
+            request_user_reset_password_token = request.data['user_token']
+
+            Password.change_user_password(request_user_new_password, request_user_reset_password_token)
+
+            response = EndpointsUtils.create_message_endpoint_response(
+                        status = EndpointsUtils.status_success, 
+                        message = "User password has been changed")
+
+            return Response(response, status.HTTP_200_OK)
+        except Exception as error:
+            response = EndpointsUtils.create_message_endpoint_response(
+                        status = EndpointsUtils.status_error,
+                        message = str(error))
+
+            return Response(response, status.HTTP_400_BAD_REQUEST)
---
+++ users/models.py
@@ -0,0 +1,260 @@
+from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ObjectDoesNotExist
+from datetime import timedelta
+
+from mec_energia import settings
+
+from universities.models import ConsumerUnit, University
+
+from .managers import CustomUserManager
+
+
+class UserTokenManager(models.Manager):
+    def valid_tokens(self):
+        expiration_time = timezone.now() - timedelta(minutes=settings.RESET_PASSWORD_TOKEN_TIMEOUT)
+
+        return self.filter(created_at__gt = expiration_time)
+    
+    def get_user_users_waiting_to_send_email(self):
+        expiration_time = timezone.now() - timedelta(minutes=settings.RESEND_EMAIL_RESET_PASSWORD_TIMEOUT)
+        tokens = self.filter(invalid_tried_at__gt = expiration_time)
+
+        return CustomUser.objects.filter(id__in = tokens.values_list('user_id', flat=True))
+    
+
+class UserToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    invalid_tried_at = models.DateTimeField(null=True, blank=True)
+
+    objects = UserTokenManager()
+
+    @property
+    def is_valid_token(self):
+        expiration_time = self.created_at + timedelta(minutes = settings.RESET_PASSWORD_TOKEN_TIMEOUT)
+        return timezone.now() < expiration_time
+    
+    def set_invalid_tried_datetime_to_send_new_email(self):
+        if not self.invalid_tried_at:
+            self.invalid_tried_at = timezone.now()
+            self.save()
+    
+    @classmethod
+    def get_user_by_token(cls, token):
+        try:
+            user_token = UserToken.objects.get(token = token)
+
+            if not user_token.is_valid_token:
+                raise Exception('Token inválido')
+
+            return user_token.user
+        except UserToken.DoesNotExist:
+            raise Exception('Token inválido')
+        
+    @classmethod
+    def get_user_by_token_and_set_invalid_tried(cls, token):
+        from .authentications import code_password_token_ok, code_password_token_expired
+
+        try:
+            user_token = UserToken.objects.get(token = token)
+
+            if user_token.user.account_password_status == 'OK':
+                raise Exception('Usuário já tem uma senha de acesso')
+
+            if user_token.is_valid_token:
+                return user_token.user, code_password_token_ok
+            else:
+                user_token.set_invalid_tried_datetime_to_send_new_email()
+                
+                return user_token.user, code_password_token_expired
+
+        except UserToken.DoesNotExist:
+            raise Exception('Token não existe ou já utilizado')
+
+    @classmethod
+    def get_enable_user_token_by_user(cls, user):
+        valid_token = cls.objects.valid_tokens().filter(user=user)
+        return valid_token.exists()
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    
+    ### User types
+    super_user_type = 'super_user'
+    university_admin_user_type = 'university_admin'
+    university_user_type = 'university_user'
+
+    all_user_types = [
+        super_user_type,
+        university_admin_user_type,
+        university_user_type
+    ]
+
+    university_user_types = [
+        university_admin_user_type,
+        university_user_type
+    ]
+
+    user_types = (
+        (super_user_type, 'super_user'),
+        (university_admin_user_type, 'university_admin'),
+        (university_user_type,'university_user'),
+    )
+
+    username = None
+    
+    first_name = models.CharField(
+        max_length=25
+    )
+
+    last_name = models.CharField(
+        max_length=25
+    )
+
+    email = models.EmailField(
+        _('Email is required'),
+        unique=True,
+        null=False
+    )
+
+    type = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False,
+        choices=user_types
+    )
+
+    password_status = (
+        ('OK', 'OK'),
+        ('first_access', 'first_access'),
+        ('admin_reset', 'admin_reset'),
+        ('user_reset', 'user_reset'),
+    )
+
+    account_password_status = models.CharField(
+        default='OK',
+        max_length=25,
+        null=False,
+        blank=False,
+        choices=password_status
+    )
+
+    @property
+    def have_reset_password_token_enable(self) -> bool:
+        return UserToken.get_enable_user_token_by_user(user = self)
+
+    def __str__(self):
+        return f'Token for {self.user.username}'
+
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+        
+    def change_user_password(self, current_password, new_password):
+        if not self.check_password(current_password):
+            raise Exception('Erro ao alterar senha: senha atual incorreta')
+        self.set_password(new_password)
+        self.save()
+
+    @classmethod
+    def search_user_by_id(cls, id):
+        try:
+            user = cls.objects.get(id = id)
+
+            return user
+        except ObjectDoesNotExist:
+            raise Exception('User does not exist')
+
+    @classmethod
+    def search_user_by_email(cls, email):
+        try:
+            user = cls.objects.get(email = email)
+
+            return user
+        except ObjectDoesNotExist as error:
+            raise error
+
+    def change_user_password_by_reset_password_token(self, new_password, reset_password_token):
+        from .authentications import Password
+
+        try:
+            Password._get_user_by_token(reset_password_token)
+            
+            self.set_password(new_password)
+            self.save()
+
+            return self
+        except Exception as error:
+            raise Exception(str(error))
+        
+    def set_account_password_status_to_ok(self):
+        self.account_password_status = 'OK'
+        self.save()
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name} [{self.email}]'
+
+
+class UniversityUser(CustomUser):
+    university_admin_user_type = CustomUser.university_admin_user_type
+    university_user_type = CustomUser.university_user_type
+
+    favorite_consumer_units = models.ManyToManyField(ConsumerUnit, blank=True)
+    
+    university = models.ForeignKey(
+        University,
+        blank=False,
+        null=False,
+        on_delete=models.PROTECT,
+        verbose_name='Universidade',
+        help_text=_(
+            'Um Usuário de Universidade deve estar ligado a uma Universidade')
+    )
+
+    def add_or_remove_favorite_consumer_unit(self, consumer_unit_id: int | str, action: str):
+        unit = ConsumerUnit.objects.get(pk=consumer_unit_id)
+
+        if unit.university.id != self.university.id:
+            raise Exception('Cannot add/remove consumer unit from another university')
+
+        if action == 'add':
+            self.favorite_consumer_units.add(unit)
+        elif action == 'remove':
+            self.favorite_consumer_units.remove(unit)
+        else:
+            raise Exception('"action" field must be "add" or "remove"')
+
+    def get_user_favorite_consumer_units(self):
+        return self.favorite_consumer_units.all()
+    
+    def check_if_consumer_unit_is_your_favorite(self, consumer_unit_id):
+        favorite_consumer_units = self.get_user_favorite_consumer_units()
+
+        if favorite_consumer_units.filter(id = consumer_unit_id):
+            return True
+        
+        return False
+
+    def change_university_user_type(self, new_user_type):
+        if not new_user_type in CustomUser.university_user_types:
+            raise Exception('New University User type does not exist')
+        
+        if not self.type in CustomUser.university_user_types:
+            raise Exception('User is not User University')
+        
+        if self.type == CustomUser.university_admin_user_type:
+            admin_university_users = UniversityUser.objects.all().filter(university = self.university, type = CustomUser.university_admin_user_type)
+            
+            if len(admin_university_users) == 1:
+                raise Exception('This User is the last Admin University User')
+        
+        self.type = new_user_type
+        self.save()
